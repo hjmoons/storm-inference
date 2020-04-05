cc800fa16e0415be77da00e7b6950ecc7044ef43
HyoJong Moon <gywhd408@naver.com>
2020-04-05 22:23:24 +0900

    add to load inference model in InferenceBolt

--- src/main/java/dke/model/InferenceBolt.java
+++ src/main/java/dke/model/InferenceBolt.java
@@ -1,17 +1,42 @@
 package dke.model;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
+import org.springframework.core.io.ClassPathResource;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Map;
 
 public class InferenceBolt extends BaseRichBolt {
+    private Log LOG = LogFactory.getLog(InferenceBolt.class);
+
+    private OutputCollector outputCollector;
+    private SavedModelBundle savedModelBundle;
+    private Session sess;
+
     @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
 
+        ClassPathResource resource = new ClassPathResource("models/cnn/saved_model.pb");
+
+        try {
+            File modelFile = new File("./saved_model.pb");
+            IOUtils.copy(resource.getInputStream(), new FileOutputStream(modelFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        savedModelBundle = SavedModelBundle.load("./", "serve");
+        sess = savedModelBundle.session();
     }
 
     @Override
